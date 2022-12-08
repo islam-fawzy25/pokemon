4fa1ad7df5534275661cbaf970e1fd426c5bd499
Islam <islam.fawzy25@hotmail.com>
2022-12-08 20:40:38 +0100

    use routes istesd of switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes ,HashRouter} from 'react-router-dom';
 // eslint-disable-next-line no-useless-rename
 import PokemonPage from './client/pages/pokemons-page/Pokemons.page';
 import SinglePokemonPage from './client/pages/single-pokemon-page/SinglePokemon.page';
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<PokemonPage />} />
         <Route path="/pokemon/:name" element={<SinglePokemonPage />} />
       </Routes>
+
     </div>
 
 
