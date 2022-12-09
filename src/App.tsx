450adf70b4e90c5b6a6c715df1c706f64cce2941
Islam <islam.fawzy25@hotmail.com>
2022-12-09 21:41:32 +0100

    add router and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes ,HashRouter} from 'react-router-dom';
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
 // eslint-disable-next-line no-useless-rename
 import PokemonPage from './client/pages/pokemons-page/Pokemons.page';
 import SinglePokemonPage from './client/pages/single-pokemon-page/SinglePokemon.page';
@@ -8,10 +8,13 @@ function App() {
   return (
     <div>
 
-      <Routes>
-        <Route path="/" element={<PokemonPage />} />
-        <Route path="/:name" element={<SinglePokemonPage />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PokemonPage />} />
+          <Route path="pokemon/:name" element={<SinglePokemonPage />} />
+        </Routes>
+      </BrowserRouter>
+
 
     </div>
 
