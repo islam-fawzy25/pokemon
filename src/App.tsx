227cac1087bee5f33bb4c98e4f039029854eff23
Islam <islam.fawzy25@hotmail.com>
2022-12-18 12:42:05 +0100

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // eslint-disable-next-line no-useless-rename
 import PokemonPage from './client/pages/pokemons-page/Pokemons.page';
 import SinglePokemonPage from './client/pages/single-pokemon-page/SinglePokemon.page';
@@ -8,14 +8,13 @@ function App() {
   return (
     <div>
 
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<PokemonPage />} />
           <Route path="pokemon/:name" element={<SinglePokemonPage />} />
         </Routes>
       </BrowserRouter>
- */}
-          <Route path="/" element={<PokemonPage />} />
+
 
     </div>
 
